3dadfe0623985710ea4dc7e530c90e0c03102930
Srivatsan V <srivi567@gmail.com>
2019-08-24 16:56:50 +0530

    Made changes - added a index route and added a start script for enabling deployment

--- node-blazor-api/index.js
+++ node-blazor-api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv/config");
 const app = express();
 const PORT = process.env.PORT || 8080;
-const million = require("./modals/db");
+const million = require("./models/db");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
@@ -15,12 +15,17 @@ mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
   console.log("db connected");
 });
 
+app.get("/", (req, res) => {
+  res.status(200).send("index");
+});
+
 app.get("/:id1/:id2", async (req, res) => {
   try {
     const data = await million
       .find()
       .skip(parseInt(req.params.id1))
       .limit(parseInt(req.params.id2));
+    console.log(data);
     res.status(200).json(data);
   } catch (err) {
     res.status(404).json({ message: err });

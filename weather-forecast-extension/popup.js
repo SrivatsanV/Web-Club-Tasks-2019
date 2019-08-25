7706b0f8bc364b74d8c4f4f32aed11fc6f6a8f54
Srivatsan V <srivi567@gmail.com>
2019-08-25 17:55:19 +0530

    Added functionality to click each forecast button to display more information for that time.
    Font is changed as well.

--- weather-forecast-extension/popup.js
+++ weather-forecast-extension/popup.js
@@ -4,14 +4,15 @@
 
 "use strict";
 
+let $location = $("#location");
 let $info = $("#info");
-let $wea = $("#wea");
-let Weather;
+let $weatherButtons = $("#weather-buttons");
+let weather;
+let i = 0;
+
 navigator.geolocation.getCurrentPosition(position => {
   fetch(
-    `http://api.openweathermap.org/data/2.5/forecast?lat=${
-      position.coords.latitude
-    }&lon=${position.coords.longitude}&APPID=e9748f3f89410e0e40d1b2185c2d9b46`
+    `http://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&APPID=e9748f3f89410e0e40d1b2185c2d9b46`
   )
     .then(function(response) {
       if (response.status !== 200) {
@@ -21,51 +22,82 @@ navigator.geolocation.getCurrentPosition(position => {
         return;
       }
 
-      // Examine the text in the response
+      // Parse response into json
       response.json().then(function(data) {
         console.log(data);
         let city = data.city;
-        let weather = data.list;
+        weather = data.list;
+
+        //storing the city and weather details in local storage
         chrome.storage.local.set({ city: city, weather: weather }, function() {
+          //retreiving data from local storage
           chrome.storage.local.get(["city"], function(result) {
             let city = result.city;
-            //console.log(result.data);
-            let html = "<h4>" + city.name + "</h4>";
-            $info.append(html);
-            //   changeColor.style.backgroundColor = data.color);
-            // changeColor.setAttribute("value", data.color);
+            let html = "<h4>Location: " + city.name + "</h4>";
+            $location.append(html);
           });
           chrome.storage.local.get(["weather"], function(result) {
-            let i = 0;
-            let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-
+            let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]; //for displaying a day for a given date
             weather.forEach(w => {
-              let cardId = "card" + i;
-              let card = document.createElement("div");
-              card.setAttribute("id", cardId);
+              //a card element to display day, the icon and temperature
+              let card = document.createElement("button");
+              card.setAttribute("id", i);
               card.setAttribute("class", "cardUI");
 
+              //icon element
               let icon = document.createElement("img");
               icon.setAttribute("class", "icon");
 
+              //temperature - shown in degree celcius
               let t_cel = Math.floor(w.main.temp - 273);
-              let h = "<h5>" + t_cel + "<span>&#8451;</span></h5>";
+              let h = "<h5>" + t_cel + "<span>&deg;C</span></h5>";
 
-              let url = `http://openweathermap.org/img/wn/${
-                w.weather[0].icon
-              }@2x.png`;
+              let url = `http://openweathermap.org/img/wn/${w.weather[0].icon}@2x.png`;
               icon.src = url;
               let date = new Date(w.dt_txt);
               let day = "<h4>" + days[date.getDay()] + "</h4>";
               let time = date.getHours() + ":00 hrs";
-              let timeHtml = "<h6>" + time + "</h6>";
+              let timeHtml = "<h5>" + time + "</h5>";
+
               $(card).append(day);
               $(card).append(icon);
               $(card).append(h);
               $(card).append(timeHtml);
-              $wea.append(card);
+              $weatherButtons.append(card);
               i++;
             });
+            if (i >= 39) {
+              $("#0").addClass("selected");
+              let item = weather[0];
+
+              let icon = document.createElement("img");
+              icon.setAttribute("class", "icon-info");
+              let url = `http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`;
+              icon.src = url;
+
+              let min_temp = Math.floor(item.main.temp_min - 273);
+              let max_temp = Math.floor(item.main.temp_max - 273);
+              let html = document.createElement("div");
+              html.setAttribute("id", "weather");
+              let innHtml =
+                "<h5>Min Temp:   " +
+                min_temp +
+                "<span>&deg;C</span>&emsp;Max Temp: " +
+                max_temp +
+                "<span>&deg;C</span></h5>" +
+                "<h5>Pressure: " +
+                item.main.pressure +
+                "Pa</h5>" +
+                "<h5>Humidity: " +
+                item.main.humidity +
+                "%</h5>" +
+                "<h5>Forecast: " +
+                item.weather[0].description +
+                "</h5>";
+              $(html).append(innHtml);
+              $info.append(icon);
+              $info.append(html);
+            }
           });
         });
       });
@@ -74,3 +106,49 @@ navigator.geolocation.getCurrentPosition(position => {
       console.log("Fetch Error :-S", err);
     });
 });
+
+check();
+function check() {
+  if ($("button").is(":visible")) {
+    $("button").click(function() {
+      $("button").removeClass("selected");
+      $(this).addClass("selected");
+      let id = parseInt(this.id);
+      let item = weather[id];
+
+      $("#weather").remove();
+      $(".icon-info").remove();
+
+      let icon = document.createElement("img");
+      icon.setAttribute("class", "icon-info");
+      let url = `http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`;
+      icon.src = url;
+      let html = document.createElement("div");
+      html.setAttribute("id", "weather");
+
+      let min_temp = Math.floor(item.main.temp_min - 273);
+      let max_temp = Math.floor(item.main.temp_max - 273);
+      let innHtml =
+        "<h5>Min Temp: " +
+        min_temp +
+        "<span>&deg;C</span>&emsp;Max Temp: " +
+        max_temp +
+        "<span>&deg;C</span></h5>" +
+        "<h5>Pressure: " +
+        item.main.pressure +
+        "Pa</h5>" +
+        "<h5>Humidity: " +
+        item.main.humidity +
+        "%</h5>" +
+        "<h5>Forecast: " +
+        item.weather[0].description +
+        "</h5>";
+      $(html).append(innHtml);
+      $info.append(icon);
+      $info.append(html);
+    });
+    return;
+  } else {
+    setTimeout(check, 50);
+  }
+}
